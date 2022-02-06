3dfa2fe23003043d63af77f8ff360674a477b374
Roger Hartley <roger.hartley@apdcomms.co.uk>
2022-02-06 15:50:27 +0000

    Add console app

---
+++ MeterReadingApp/MeterReadingConsoleApp/PostMeterReadingsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MeterReadingConsoleApp
+{
+    internal record PostMeterReadingsResponse
+    {
+        public int SuccessfulReadings { get; init; }
+
+        public int FailedReadings { get; init; }
+    }
+}
---
+++ MeterReadingApp/MeterReadingConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿const string RootUri = "http://localhost:5075";
+
+using var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri(RootUri);
+
+var csvFileBytes = File.ReadAllBytes("Meter_Reading.csv");
+using var byteArrayContent = new ByteArrayContent(csvFileBytes);
+
+using var multipartContent = new MultipartFormDataContent();
+multipartContent.Add(byteArrayContent, "meterReadingsFile", "meterReadingsFile");
+
+using var response = await httpClient.PostAsync("MeterReading", multipartContent);
+var meterReadingsResponse = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine($"Api returned status code {response.StatusCode}");
+Console.WriteLine(meterReadingsResponse);
+Console.ReadLine();
